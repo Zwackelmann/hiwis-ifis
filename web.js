50b754aba6a92a2e125a7759473b3245f08110bf
Simon <simon@simon-Laptop.(none)>
2011-10-18 16:25:42 +0200

    init

---
+++ web.js
@@ -0,0 +1,21 @@
+/*
+ * git init
+ * git add .
+ * git commit -a -m "init"
+ * heroku create --stack cedar
+ * git push heroku master
+ * heroku ps:scale web=1
+ * heroku ps
+ * heroku config:add NODE_ENV=production
+ */
+
+var express = require('express');
+var app = express.createServer(express.logger());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
