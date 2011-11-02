54e653e07469b67b05e39b526072f929c53374d5
phil <philipp.wille@web.de>
2011-11-02 19:16:48 +0100

    Added comment crud markup and ajax calls for deleting comments.

--- controller/CommentController.js
+++ controller/CommentController.js
@@ -21,12 +21,27 @@ comment.post('/create', function(request, response) {
   });
 });
 
+// TODO: comment.delete(...)
 comment.get('/delete', Controller.requiresLogin, function(request, response) {
   response.writeHead(200, {'Content-Type': 'application/json'});
+  var postId = request.param("postId");
+  if(postId == null) {
+    console.log("Error while removing a comment: Post id was null!");
+    response.end();
+    return;
+  }
   
-  Post.findOne({_id: request.param("postId")}, function(err, post) {
-    post.comments.id(request.param("commentId")).remove();
+  Post.findOne({_id: postId}, function(err, post) {
+    var commentId = request.param("commentId")
+      , comment = post.comments.id(commentId);
     
+    if(comment == null) {
+      console.log("Error while removing a comment: Comment with id " + commentId + " not found!");
+      response.end();
+      return;
+    }
+    
+    comment.remove();
     post.save(function(err, res) {
       response.end(JSON.stringify({err: err}));
     });
