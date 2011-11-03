9becd5a14a463bfa2f637d3b1c5d07ddbe7cfe2d
Simon <simon@simon-Laptop.(none)>
2011-11-03 08:02:18 +0100

    added some database configuration, so that it runs on heroku and localhost

--- web.js
+++ web.js
@@ -9,15 +9,24 @@
  * heroku config:add NODE_ENV=production
  */
 
-var mongoose = require('mongoose')
-  , database_url = process.env.DATABASE_URL || 'mongodb://localhost/hiwis-ifis'
-  , db = mongoose.connect(database_url, function(err){if(err)console.log(err);})
-  // , db = mongoose.connect('mongodb://localhost/hiwis-ifis')
-  , models = require('./models')(db)
-  , dummys = require('./models/dummys')
-  , express = require('express')
-  , app = express.createServer();
+// Config vars
+var NODE_ENV;
+if(typeof process.env.NODE_ENV == 'string' && process.env.NODE_ENV.toLowerCase() == 'production') {
+  NODE_ENV = 'production';
+} else {
+  NODE_ENV = 'development';
+}
 
+var database_url = process.env.DATABASE_URL || 'mongodb://localhost/hiwis-ifis';
+
+// Initialize modules
+var mongoose = require('mongoose')
+  , db = mongoose.connect(database_url, function(err){if(err)console.log(err);})
+  , models = require('./models')(db)
+  , express = require('express');
+
+// Create and Configure Server
+app = express.createServer();
 app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.static(__dirname + '/static'));
@@ -30,6 +39,7 @@ app.configure(function() {
   app.use(express.methodOverride()); // TODO: Really use it
 });
 
+// Define some classes as globals
 global.CallbackAfterN = require('./src/CallbackAfterN');
 global.User = models.User;
 global.Post = models.Post;
@@ -37,20 +47,26 @@ global.Comment = models.Comment;
 global.Controller = require('./src/Controller')(app);
 
 
-dummys.init(models);
-var bootstrap = require('./bootstrap');
+// Execute Bootstrap if node is in development mode
+if(NODE_ENV == 'development') {
+  var dummys = require('./models/dummys');
+  dummys.init(models);
+  var bootstrap = require('./bootstrap');
+  
+  bootstrap.init(models);
+  bootstrap.down(function(){
+    bootstrap.up();
+  });
+  
+  global.dummys = dummys;
+}
 
-bootstrap.init(models);
-bootstrap.down(function(){
-  bootstrap.up();
-});
-
-global.dummys = dummys;
-
+// Initialize Controllers
 var PostController = require('./controller/PostController');
 var CommentController = require('./controller/CommentController');
 var SessionController = require('./controller/SessionController');
 
+// define some default pages
 app.get('/', function(request, response) {
   Post
     .find({ published: true })
@@ -72,8 +88,8 @@ app.get('/', function(request, response) {
   });
 });
 
+// start server
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-  console.log("Using database url: " + database_url);
 });
