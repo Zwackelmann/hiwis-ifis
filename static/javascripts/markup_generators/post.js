eb4061106ba7e5290c8196d63cd773e183f5c1ec
Simon <simon@simon-Laptop.(none)>
2011-11-03 08:30:39 +0100

    fixed a little minor bug so that posts can be deleted again

--- static/javascripts/markup_generators/post.js
+++ static/javascripts/markup_generators/post.js
@@ -20,7 +20,7 @@ function generatePostMarkup(post) {
   
   var markup = "" +
   "<article class=\"post\">" +
-  "    <a class=\"iconic red\" href=\"javascript:\" data-onclick=\"remove(getPost($(this)))\">x</a>" +
+  "    <a class=\"iconic red\" href=\"javascript:\" onClick=\"remove(getPost($(this)));\">x</a>" +
   "    <a class=\"iconic\" href=\"#" + post._id + "_attributes\">p</a>" +
   "    <time datetime=\"" + date.toString() + "\" pubdate=\"pubdate\">" +
   "        <ul>" +
